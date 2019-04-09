9ec41b4877f0d2694079cf7f9859a6ea6fb67939
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-09 14:06:09 -0400

    investigateing tree() function

--- assignments.js
+++ assignments.js
@@ -6,16 +6,16 @@ const inspectOptions = {
   colors: true,
 };
 
-// const alpha = new BST();
+const alpha = new BST();
 
-// alpha.insert(3, 'foo');
-// alpha.insert(1, 'foo');
-// alpha.insert(4, 'foo');
-// alpha.insert(6, 'foo');
-// alpha.insert(9, 'foo');
-// alpha.insert(2, 'foo');
-// alpha.insert(5, 'foo');
-// alpha.insert(7, 'foo');
+alpha.insert(3, '1');
+alpha.insert(1, '2');
+alpha.insert(4, '2');
+alpha.insert(6, '3');
+alpha.insert(9, '4');
+alpha.insert(2, '3');
+alpha.insert(5, '4');
+alpha.insert(7, '5');
 
 // alpha.remove(3);
 
@@ -24,17 +24,29 @@ const inspectOptions = {
 
 const bravo = new BST();
 
-bravo.insert('E', 'foo');
-bravo.insert('A', 'foo');
-bravo.insert('S', 'foo');
-bravo.insert('Y', 'foo');
-bravo.insert('Q', 'foo');
-bravo.insert('U', 'foo');
-bravo.insert('E', 'foo');
-bravo.insert('S', 'foo');
-bravo.insert('T', 'foo');
-bravo.insert('I', 'foo');
-bravo.insert('O', 'foo');
-bravo.insert('N', 'foo');
+bravo.insert('E', 'e');
+bravo.insert('A', 'a');
+bravo.insert('S', 's');
+bravo.insert('Y', 'y');
+bravo.insert('Q', 'q');
+bravo.insert('U', 'u');
+bravo.insert('E', 'e');
+bravo.insert('S', 's');
+bravo.insert('T', 't');
+bravo.insert('I', 'i');
+bravo.insert('O', 'o');
+bravo.insert('N', 'n');
 
-console.log(util.inspect(bravo, inspectOptions));
+// console.log(util.inspect(bravo, inspectOptions));
+
+
+// It outputs the tree structure in plain text, however it's hard read without
+// the added parenthesis we added to the return statement;
+function tree(t){
+  if(!t){
+    return 0;
+  }
+  return '(' + tree(t.left) + t.value + tree(t.right) + ')';
+}
+
+console.log(tree(alpha));
